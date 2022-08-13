eea7b75afa4c3e72bb65e22b613910aa4585f4ff
bcrist3 <bcrist@unomaha.edu>
2022-08-12 19:48:33 -0500

    Added Comments to the file

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from tkinter import ttk
 
 def main():
     window = Tk()
+    # Sets the window name
     window.title('Calculator')
+    # Sets the window geometry
     window.geometry('500x600')
+    # Makes the window unable to be resizabled
     window.resizable(False, False)
+    # Creates a notebook
     notebook = ttk.Notebook(window)
     widgets = GUI(window, notebook)
     window.mainloop()
